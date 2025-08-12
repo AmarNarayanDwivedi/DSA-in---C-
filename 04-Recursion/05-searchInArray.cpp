6032084e17dd981685112ad4327bcbdf12fc5f0e
Amar Narayan Dwivedi <amardwivedi792@gmail.com>
2025-08-12 10:48:51 +0530

    Recursion Basic Problems

--- 04-Recursion/05-searchInArray.cpp
+++ 04-Recursion/05-searchInArray.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+bool searchInArray(int arr[], int index, int size, int target)
+{
+    if (index == size)
+    {
+        return false;
+    }
+    if (arr[index] == target)
+        return true;
+    searchInArray(arr, index + 1, size, target);
+}
+
 int main()
 {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int index = 0;
+    int size = 7;
+    int target = 1;
 
+    bool ans = searchInArray(arr, index, size, target);
+    cout << ans << endl;
     return 0;
 }
